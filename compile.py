1cdb790757620c100e8d2fba664fd935224f9dca
sophie_desu <3203911711@qq.com>
2021-06-05 12:09:54 +0800

    Update compile.py

--- compile.py
+++ compile.py
@@ -1,2 +1,2 @@
-from py_compile import compile
-compile('Multi_threaded_download_library.py', cfile=None, dfile=None, doraise=True, optimize=-1)
+from py_compile import compile as py_compile
+py_compile('Multi_threaded_download_library.py', cfile=None, dfile=None, doraise=True, optimize=-1)
